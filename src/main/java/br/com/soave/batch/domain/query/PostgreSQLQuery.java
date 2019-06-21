ec44784c6b694fdef9adba0fb967fdfa8d37ef54
Arthur Soave Oliveira de Paula <atrpaula@gmail.com>
2019-06-21 18:45:40 -0300

    Fix insert query

--- src/main/java/br/com/soave/batch/domain/query/PostgreSQLQuery.java
+++ src/main/java/br/com/soave/batch/domain/query/PostgreSQLQuery.java
@@ -5,18 +5,18 @@ public class PostgreSQLQuery {
     public static final String writeProductsPostgreSQL(){
         StringBuilder query = new StringBuilder();
         query.append("   INSERT INTO                  ");
-        query.append("   BATCHDB.PRODUCTS             ");
+        query.append("   BATCHDB.PUBLIC.PRODUCTS      ");
         query.append("           (ID_PRODUCT,         ");
-        query.append("           (NAME_PRODUCT,       ");
-        query.append("           (COST_PRICE_PRODUCT, ");
-        query.append("           (SALE_PRICE_PRODUCT, ");
-        query.append("           (BARCODE_PRODUCT)    ");
+        query.append("            NAME_PRODUCT,       ");
+        query.append("            COST_PRICE_PRODUCT, ");
+        query.append("            SALE_PRICE_PRODUCT, ");
+        query.append("            BARCODE_PRODUCT)    ");
         query.append("   VALUES                       ");
         query.append("           (:productId,         ");
-        query.append("           (:productName,       ");
-        query.append("           (:costPriceProduct,  ");
-        query.append("           (:salePriceProduct,  ");
-        query.append("           (:barCodeProduct)    ");
+        query.append("            :productName,       ");
+        query.append("            :costPriceProduct,  ");
+        query.append("            :salePriceProduct,  ");
+        query.append("            :barCodeProduct)    ");
         return query.toString();
     }
 }
